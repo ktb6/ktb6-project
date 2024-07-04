2a26b2692225bb22acf3e1862fffc42114764de2
midamidaa <81531278+midamidaa@users.noreply.github.com>
2024-07-04 20:06:10 +0900

    KTB-14: minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,16 +16,22 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="container mx-auto p-4">
-        <h1 className="text-4xl font-bold mb-4">육두문자 Team Blog</h1>
+        <h1 className="text-4xl mb-4">Latest Posts</h1>
         <ul>
           {posts.map((post) => (
-            <li key={post.slug} className="mb-4">
+            <div key={post.slug} className="container mb-r p-4 hover:bg-gradient-to-r from-gray-200 to-gray-300">
+            <li
+              key={post.slug}
+            >
               <Link href={`/posts/${post.slug}`}>
-                <span className="text-blue-500 text-2xl">{post.title}</span>
+                <span className="text-blue-500 text-2xl mb-2 block">
+                  {post.title}
+                </span>
               </Link>
-              <p className="text-gray-600">{post.date}</p>
+              <p className="text-gray-600 mb-2">{post.date}</p>
               <p>{post.description}</p>
             </li>
+            </div>
           ))}
         </ul>
       </div>
