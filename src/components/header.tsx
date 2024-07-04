41c15c288a4d70e6cebeee7d76c9346ea64e6a00
Danny-Jo <dannyjo.world@gmail.com>
2024-07-04 17:12:30 +0900

    KTB-1 feat: blog 기본 틀 업데이트

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import Link from "next/link";
 
-export const Header = () => {
+const Header = () => {
   return (
-    <header className="h-10 w-full px-8 flex items-center justify-between">
-      <div className="text-2xl">육두문자 Team Blog</div>
-      <nav className="flex gap-2">
-        <div>Post</div>
-        <div>About</div>
-      </nav>
+    <header className="bg-transparent text-black p-4">
+      <div className="container mx-auto flex justify-between items-center text-white">
+        <h1 className="text-2xl font-bold">
+          <Link href="/">
+            <span>Team Blog</span>
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/">
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
+
+export default Header;
