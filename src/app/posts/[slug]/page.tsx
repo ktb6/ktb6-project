5485f9c893ea1d13dfe430c154da528bf92d9c5e
Danny-Jo <dannyjo.world@gmail.com>
2024-07-10 01:47:07 +0900

    [KTB-15] lint: Image alt, src 관련 코드 lint

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,7 +3,6 @@ import path from 'path';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
-import { ImgHTMLAttributes } from 'react';
 
 export async function generateStaticParams() {
   const postsDirectory = path.join(process.cwd(), 'public/posts');
@@ -34,31 +33,31 @@ export default function Post({ params }: PostProps) {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="container max-w-2xl mx-auto p-4">
-          <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
-          {/* <p className="text-gray-600 mb-8">{data.date}</p> */}
-          <div className="text-gray-600 mb-8 flex justify-between">
-            <span>{data.date}</span>
-            <span>{data.author}</span>
-          </div>
-          <p className="text-gray-800 mb-8">{data.description}</p>
-          <div className="prose">
-            <ReactMarkdown
-              components={{
-                img: ({ node, ...props }) => (
-                  <Image
-                    alt={props.alt || ""}
-                    src={props.src || ""}
-                    layout="responsive"
-                    width={700}
-                    height={475}
-                    className="mx-auto"
-                  />
-                ),
-              }}
-            >
-              {content}
-            </ReactMarkdown>
-          </div>
+        <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
+        {/* <p className="text-gray-600 mb-8">{data.date}</p> */}
+        <div className="text-gray-600 mb-8 flex justify-between">
+          <span>{data.date}</span>
+          <span>{data.author}</span>
+        </div>
+        <p className="text-gray-800 mb-8">{data.description}</p>
+        <div className="prose">
+          <ReactMarkdown
+            components={{
+              img: ({ alt, src }) => (
+                <Image
+                  alt={alt || ''}
+                  src={src || ''}
+                  layout="responsive"
+                  width={700}
+                  height={475}
+                  className="mx-auto"
+                />
+              ),
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   );
