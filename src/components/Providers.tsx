71f04460949e8d2c705dad690b839fb9cc8c6d52
midamidaa <81531278+midamidaa@users.noreply.github.com>
2024-07-19 15:08:07 +0900

    providers.tsx export default 형식으로 변경

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,10 +2,12 @@
 
 import { ThemeProvider } from 'next-themes';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </ThemeProvider>
   );
-}
+};
+
+export default Providers;
